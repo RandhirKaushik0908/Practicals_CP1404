e9158d4b09bc47a9144f1d7323142faaa6bd95fe
RandhirKaushik0908 <randhir.kaushik@my.jcu.edu.au>
2020-06-01 01:49:43 +1000

    Completed cleanup_files.py

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -9,19 +9,15 @@ import os
 def main():
     """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
-
     # Change to desired directory
     os.chdir('Lyrics/Christmas')
-
     # Print a list of all files in current directory
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
     # Make a new directory
     try:
         os.mkdir('temp')
     except FileExistsError:
         pass
-
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
         # Ignore directories, just process files
@@ -40,7 +36,25 @@ def main():
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = filename
+    for x, y in enumerate(new_name):
+        try:
+            if new_name[x].islower() and new_name[x + 1].isupper():
+                new_name = new_name.replace("{}{}".format(new_name[x], new_name[x + 1]),
+                                            "{}_{}".format(new_name[x], new_name[x + 1]))
+            if new_name[x] == ' ':
+                if new_name[x + 1].isalpha():
+                    new_name = new_name.replace(" {}".format(new_name[x + 1]), "_{}".format(new_name[x + 1].title()))
+                elif new_name[x + 1] == '(' and new_name[x + 2].isalpha():
+                    new_name = new_name.replace(" ({}".format(new_name[x + 2]), "_({}".format(new_name[x + 2].title()))
+            if new_name[x] == '.' and new_name[x + 1] == 'T' and new_name[x + 2] == 'X' and new_name[x + 3] == 'T':
+                new_name = new_name.replace("{}{}{}{}".format(new_name[x], new_name[x + 1], new_name[x + 2],
+                                                              new_name[x + 3]), ".txt")
+            if new_name[x].isupper() and new_name[x + 1].isupper():
+                new_name = new_name.replace("{}".format(new_name[x]), "{}_".format(new_name[x]))
+        except IndexError:
+            pass
+    print(new_name)
     return new_name
 
 
